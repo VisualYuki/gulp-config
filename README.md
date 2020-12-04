a7df871b25e5248fb46cff0fd25ba27c1b31d16e
Den <den.is@rootmedia.ru>
2020-12-04 22:11:20 +0300

    fix

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # Front end
 
 ## Начальная настройка
-***
 1. `` $ npm install `` - установить node_modules P.S должна быть установлена node.js глобально
 2. `` $ npm run getLibs или gulp getLibs `` - скачать .js и .css файлы из библиотек из node_modules P.S все файлы .css нужно переименовать .less
-2. `` $ npm update `` - обновить версии пакетов до текущего состояния
+2. `` $ npm update --save`` - обновить пакеты до актуальной версии
 
 ## Gulp задачи
-***
-1. `` $ gulp dev `` - запуск сервера для разработки и отслеживания файлов (development mode) 
-2. `` $ gulp build `` - очистка папки dist и полная компиляцией всех файлов из папки src в dist (production mode)
+1. `` $ gulp dev  или npm run dev`` - запуск сервера для разработки и отслеживания файлов (development mode) 
+2. `` $ gulp build или npm run build`` - очистка папки dist и полная компиляцией всех файлов из папки src в dist (production mode)
 
 ## Помощь
 Если совсем что-то непонятно, то можно мне написать vk - https://vk.com/moshkin_denis, или telegram - @denis_moshkin
--- gulp/svgSprite.js
+++ gulp/svgSprite.js
@@ -3,33 +3,24 @@ const rename = require("gulp-rename");
 const svgSprite = require("gulp-svg-sprite");
 const gulpif = require("gulp-if");
 
-let path = require("./config.js");
+let config = require("./config.js");
 
 module.exports = function svgsprite() {
-   let src = "src/img/**/*.svg";
-   let dist = "dist/img/sprite";
+   let src = "src/svg/**/*.svg";
+   let dist = "dist/svg/sprite";
 
    let config = {
-		
       mode: {
-         css: {
-				dest: ".",
-				view: true, 
-				prefix: "._",
-				bust: false,
-      		//cssFile: "sprite1.less",
-      		sprite: "sprite.svg",
-				layout: "horizontal",
-				dimensions: true,
-				render: {
-					less: {
-						dest: "sprite.less"
-					}
-				}
-
-         },
-      },
+         symbol: {
+            sprite: "/sprite.svg",
+            render: {
+               less: {
+                  dest:'/sprite.less'
+               }
+            }
+         }
+      }
    };
 
-   return gulp.src(src).pipe(svgSprite(config)).pipe(gulpif("*.less", gulp.dest("src/less/utils"), gulp.dest(dist)));
+   return gulp.src(src).pipe(svgSprite(config)).pipe(gulp.dest(dist));
 };
