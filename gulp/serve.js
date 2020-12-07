3828cf2ef5b45a9cc25a5ec49d5c905b749f8bdc
Den <den.is@rootmedia.ru>
2020-12-07 12:53:06 +0300

    add mixins.less

--- gulp/serve.js
+++ gulp/serve.js
@@ -3,6 +3,7 @@ const gulp = require("gulp");
 const webp = require("./img/img_webp");
 const svg = require("./img/img_svg");
 const minPng = require("./img/min_png");
+const minJpg = require("./img/min_jpg");
 const style = require("./style");
 const pug2html = require("./pug2html");
 const script = require("./script");
@@ -13,7 +14,7 @@ let config = require("./config.js");
 
 module.exports = function serve() {
    server.init({
-      server: {
+      server: { 
          baseDir: config.out.baseDir,
          directory: true,
       },
@@ -29,6 +30,7 @@ module.exports = function serve() {
    gulp.watch("src/img/**/*.{png,jpg,webp}", gulp.series(webp));
    gulp.watch("src/img/**/*.svg", gulp.series(svg));
    gulp.watch("src/img/**/*.png", gulp.series(minPng)).on("change", server.reload);
+   gulp.watch("src/img/**/*.jpg", gulp.series(minJpg)).on("change", server.reload);
    gulp.watch("src/less/**/*.less", style);
    gulp.watch("src/pug/**/*.pug", pug2html);
    //gulp.watch("src/pug/include/*.pug", include_pug2html);
@@ -36,21 +38,21 @@ module.exports = function serve() {
    gulp.watch("src/pug/include/*.pug", include_pug2html);
    gulp.watch("src/pug/layout/*.pug", include_pug2html);
    gulp.watch("src/pug/svg/*.pug", include_pug2html);
-   gulp.watch(config.out.baseDir + "pages/*.html").on("change", function (event, file) {
+   gulp.watch(config.out.baseDir + "/pages/*.html").on("change", function (event, file) {
       server.reload();
    });
-   gulp.watch(config.out.baseDir + "css/**/*.css").on("change", function (event, file) {
+   gulp.watch(config.out.baseDir + "/css/**/*.css").on("change", function (event, file) {
       //if (event === "change" || event === "add") {
       server.reload();
       //}
    });
-   gulp.watch(config.out.baseDir + "img/**/*.{png,jpg,webp,svg}").on("change", server.reload);
-   server.watch([config.out.baseDir + "img/**/*.{png,jpg,webp,svg}"], function (event, file) {
+   gulp.watch(config.out.baseDir + "/img/**/*.{png,jpg,webp,svg}").on("change", server.reload);
+   server.watch([config.out.baseDir + "/img/**/*.{png,jpg,webp,svg}"], function (event, file) {
       if (event === "change" || event === "add") {
          server.reload();
       }
    });
-   gulp.watch(config.out.baseDir + "js/**/*.js").on("change", server.reload);
+   gulp.watch(config.out.baseDir + "/js/**/*.js").on("change", server.reload);
 
 
 };
