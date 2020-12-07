efa6158e34d93b4b4626a970e1ff8260c54956df
Мошкин Денис <Moshkin.d@palax.info>
2020-12-07 18:56:02 +0300

    fix

--- gulp/serve.js
+++ gulp/serve.js
@@ -9,6 +9,7 @@ const pug2html = require("./pug2html");
 const script = require("./script");
 const include_pug2html = require("./include_pug2html");
 const server = require("browser-sync").create();
+const favicons = require("./favicons");
 
 let config = require("./config.js");
 
@@ -29,29 +30,33 @@ module.exports = function serve() {
 
    gulp.watch("src/img/**/*.{png,jpg,webp}", gulp.series(webp));
    gulp.watch("src/svg/**/*.svg", gulp.series(svg));
+   server.watch([config.out.baseDir + "/img/**/*.{png,jpg,webp,svg}"], function (event, file) {
+      if (event === "change" || event === "add") {
+         server.reload();
+      }
+   });
    gulp.watch("src/img/**/*.png", gulp.series(minPng)).on("change", server.reload);
    gulp.watch("src/img/**/*.jpg", gulp.series(minJpg)).on("change", server.reload);
+   gulp.watch(config.out.baseDir + "/svg/*.svg").on("change", server.reload);
+   gulp.watch(config.out.baseDir + "/img/**/*.{png,jpg,webp}").on("change", server.reload);
+
    gulp.watch("src/less/**/*.less", style);
+   gulp.watch(config.out.baseDir + "/css/**/*.css").on("change", function (event, file) {
+      //if (event === "change" || event === "add") {
+      server.reload();
+      //}
+   });
+
    gulp.watch("src/pug/**/*.pug", pug2html);
-   //gulp.watch("src/pug/include/*.pug", include_pug2html);
-   gulp.watch("src/js/**/*.js", script);
    gulp.watch("src/pug/include/*.pug", include_pug2html);
    gulp.watch("src/pug/layout/*.pug", include_pug2html);
    gulp.watch("src/pug/svg/*.pug", include_pug2html);
    gulp.watch(config.out.baseDir + "/pages/*.html").on("change", function (event, file) {
       server.reload();
    });
-   gulp.watch(config.out.baseDir + "/css/**/*.css").on("change", function (event, file) {
-      //if (event === "change" || event === "add") {
-      server.reload();
-      //}
-   });
-   gulp.watch(config.out.baseDir + "/img/**/*.{png,jpg,webp}").on("change", server.reload);
-   gulp.watch(config.out.baseDir + "/svg/*.svg").on("change", server.reload);
-   server.watch([config.out.baseDir + "/img/**/*.{png,jpg,webp,svg}"], function (event, file) {
-      if (event === "change" || event === "add") {
-         server.reload();
-      }
-   });
+
+   gulp.watch("src/js/**/*.js", script);
    gulp.watch(config.out.baseDir + "/js/**/*.js").on("change", server.reload);
+
+   gulp.watch("src/favicons/*.*", favicons);
 };
