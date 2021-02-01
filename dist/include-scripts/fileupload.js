9ea417dfd5e02c8e753fde4580faef3db7111573
Мошкин Денис <Moshkin.d@palax.info>
2021-02-01 13:58:00 +0300

    fix readme

--- dist/include-scripts/fileupload.js
+++ dist/include-scripts/fileupload.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-   var uppy = Uppy.Core()
-      .use(Uppy.Dashboard, {
-         inline: true,
-         target: "#fileupload",
-         locale: Uppy.locales.ru_RU
-      })
-      .use(Uppy.Tus, { endpoint: "https://tusd.tusdemo.net/files/" });
+   //var uppy = Uppy.Core()
+   //   .use(Uppy.Dashboard, {
+   //      inline: true,
+   //      target: "#fileupload",
+   //      locale: Uppy.locales.ru_RU
+   //   })
+   //   .use(Uppy.Tus, { endpoint: "https://tusd.tusdemo.net/files/" });
 
-   uppy.on("complete", (result) => {
-      console.log("Upload complete! We’ve uploaded these files:", result.successful);
-   });
+   //uppy.on("complete", (result) => {
+   //   console.log("Upload complete! We’ve uploaded these files:", result.successful);
+   //});
 });
