1c1881f37763b0b9532bffb685f8f9031c92453f
Moshkin Denis <disant1998@yandex.ru>
2021-10-29 20:16:49 +0300

    add babel conf

--- webpack.config.js
+++ webpack.config.js
@@ -37,4 +37,18 @@ module.exports = {
 		//	openAnalyzer: false,
 		//}),
 	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					//options: {
+					//	presets: ["@babel/preset-env"],
+					//},
+				},
+			},
+		],
+	},
 };
